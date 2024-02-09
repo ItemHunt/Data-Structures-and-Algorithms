b1c944bf37c99cdf8f66552d3c702b8bdeb02669
ItemHunt <littletrick883@gmail.com>
2024-02-09 12:55:50 +0800

    DFS complete

--- algorithms/search/DFS.py
+++ algorithms/search/DFS.py
@@ -1,6 +1,8 @@
 '''
 Depth First Search Algorithm
 
+Time Complexity: O(V + E) where V is the number of vertices and E is the number of edges
+
 If you want to watch a video instead I recommend this: 
 https://youtu.be/PMMc4VsIacU?si=pa59yt-6OHQTgYcZ
 
@@ -14,8 +16,6 @@ then you move back and find another path.
 
 DFS ends when you come back to the starting node
 '''
-
-# https://youtu.be/PMMc4VsIacU?si=Qg7YafgBFGooq5vm&t=572
 
 def dfs(graph, start):
     """
@@ -49,3 +49,53 @@ def dfs(graph, start):
             stack.extend(graph[vertex] - set(visited))
 
     return visited
+
+'''
+C++ Version of DFS
+------------------------------
+#include <vector>
+#include <stack>
+#include <set>
+#include <map>
+
+/*
+ * This function implements the Depth-First Search (DFS) algorithm.
+ *
+ * Parameters:
+ * graph (map<string, set<string>>): A map representing an adjacency list of the graph.
+ * start (string): The starting vertex.
+ *
+ * Returns:
+ * visited (vector<string>): A vector of vertices in the order they were visited.
+ */
+
+std::vector<std::string> dfs(std::map<std::string, std::set<std::string>> graph, std::string start) {
+    // Create a vector to store the visited vertices
+    std::vector<std::string> visited;
+
+    // Create a stack for DFS
+    std::stack<std::string> stack;
+    stack.push(start);
+
+    while (!stack.empty()) {
+        // Pop a vertex from the stack
+        std::string vertex = stack.top();
+        stack.pop();
+
+        // If the vertex is not visited before
+        if (std::find(visited.begin(), visited.end(), vertex) == visited.end()) {
+            // Mark the vertex as visited
+            visited.push_back(vertex);
+
+            // Add the vertices adjacent to the current vertex to the stack
+            for (std::string adj : graph[vertex]) {
+                if (std::find(visited.begin(), visited.end(), adj) == visited.end()) {
+                    stack.push(adj);
+                }
+            }
+        }
+    }
+
+    return visited;
+}
+'''
